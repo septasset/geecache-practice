2cff27caf26aa045f71d89e163acefa8f8e370ab
Harold Zhu <u6314203@anu.edu.au>
2022-07-21 18:15:14 +0800

    complete: enable concurrency for cache

---
+++ byteview.go
@@ -0,0 +1,28 @@
+package geecache
+
+// A ByteView holds an immutable view of bytes.
+// ByteView will be used as cache "Value", read-only prevents it to be modified unexpectedly
+type ByteView struct {
+	b []byte
+}
+
+// Len returns the view's length
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+// ByteSlice returns a copy of the data as a byte slice.
+func (v ByteView) ByteSlice() []byte {
+	return cloneBytes(v.b)
+}
+
+// String returns the data as a string, making a copy if necessary.
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	c := make([]byte, len(b))
+	copy(c, b)
+	return c
+}
